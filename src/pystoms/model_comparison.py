0c5d8639996c7c32b09903f18fd80c2e167c20dc
Tim Maier Ubuntu Workstation <tmaier01@students.uni-mainz.de>
2022-05-18 12:35:24 +0200

    model comparison violin and boxplot plot LOO

--- src/pystoms/model_comparison.py
+++ src/pystoms/model_comparison.py
@@ -3,8 +3,8 @@ This module is supposed to aid with comparison of
 two or more models.
 """
 import arviz as az
-from logging import warning
 import matplotlib.pyplot as plt
+import seaborn as sns
 import os
 import pandas as pd
 from typing import Dict
@@ -21,25 +21,56 @@ def read_idata_directory(directory_path:str) -> Dict[str,az.InferenceData]:
     """
     inference_data_dic = {}
     for idata_file in os.listdir(directory_path):
-        file_path = f"{directory_path}{idata_file}"
+        file_path = f"{directory_path}/{idata_file}"
         suffix = os.path.splitext(file_path)[1]
         if os.path.isfile(file_path) and suffix == ".nc":
-            inference_data_dic[idata_file] = az.from_netcdf(file_path)
+            idata = az.from_netcdf(file_path)
+            feature_id = idata.posterior.feature.values[0]
+            inference_data_dic[feature_id] = idata
     return inference_data_dic
 
-def compare_feature_model(models_to_compare:Dict[str,str])->pd.DataFrame:
+def compare_feature_model(models_to_compare:Dict[str,str],
+                          plot:bool = True)->pd.DataFrame:
     """Compares the performance of two
     or more peptide feature models
 
     Args:
         models_to_compare (Dict[str,str]): Dictionary with model names as
             keys and paths to inference data directory as values.
+        plot (bool, optional): Wether to plot comparison.
     Returns:
         pd.DataFrame: Dataframe with comparison stats.
     """
-    warning(f"This function is currently under construction, "
-            f"it is only comparing the first features in the directories")
-    first_features = [models_to_compare[key][0] for key in models_to_compare]
-    comparison = az.compare(first_features)
-    return comparison
+    idata_of_models = {}
+    # read in data
+    for model_name,dir_path in models_to_compare.items():
+        idata_of_models[model_name] = read_idata_directory(dir_path)
+    # from {"model_name":{"feature_id":idata, …}, …}
+    # to {"feature_id":{"model_name":idata, …}, …}
+    comparisons = {}
+    for model_name,idata_dict in idata_of_models.items():
+        for feature_id in idata_dict:
+            if feature_id not in comparisons:
+                comparisons[feature_id] = {}
+            comparisons[feature_id][model_name] = idata_dict[feature_id]
+    # run comparisons per feature and concat dataframes
+    comp_data = []
+    for feature_id, idata_dict in comparisons.items():
+        comp = az.compare(idata_dict)
+        comp["feature_id"] = feature_id
+        comp_data.append(comp)
+    comp_df = pd.concat(comp_data)
+    comp_df.reset_index(inplace=True)
+    comp_df.rename(columns={"index":"model_name"},inplace=True)
+    if plot:
+        sns.violinplot(x="model_name",
+                       y="loo",
+                       data = comp_df)
+        plt.show()
+        sns.boxplot(x="model_name",
+                    y="loo",
+                    data = comp_df,
+                    showfliers=False)
+        plt.show()
+    return comp_df
 
