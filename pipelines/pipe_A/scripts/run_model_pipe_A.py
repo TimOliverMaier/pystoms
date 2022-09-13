7347ba1ab0bd6a771f8f5e9c4ce9f65bcfa863b3
Tim Maier (Ubuntu Desktop) <tmaier01@students.uni-mainz.de>
2022-09-13 18:38:41 +0200

    pipe_A v1_1 with dvc plot histogram template 1. acceptance probability, step size, number of steps and     tree depth are recorded into json file. acceptance prob is plotted     as histogram (dvc data series) 2. inference data (idata) can be stored by pipeline if `save_traces=True` 3. Mean of divergence number and acceptance probability are recorded     as metrics for each feature

--- pipelines/pipe_A/scripts/run_model_pipe_A.py
+++ pipelines/pipe_A/scripts/run_model_pipe_A.py
@@ -1,18 +1,25 @@
 import os
 import yaml
+import json
 from proteolizarddata.data import PyTimsDataHandleDDA
 from pystoms.aligned_feature_data import AlignedFeatureData
-
+from arviz import InferenceData
 # construct output path
 file_path = os.path.dirname(__file__)
 relative_output_path = "../output_data/"
 output_path = os.path.join(file_path,relative_output_path)
 plot_path = os.path.join(output_path,"plots")
 metrics_path = os.path.join(output_path,"metrics")
+idata_path = os.path.join(output_path,"inference_data")
+plot_metrics_path = os.path.join(output_path,"plot_metrics")
 if not os.path.exists(plot_path):
     os.mkdir(plot_path)
 if not os.path.exists(metrics_path):
     os.mkdir(metrics_path)
+if not os.path.exists(idata_path):
+    os.mkdir(idata_path)
+if not os.path.exists(plot_metrics_path):
+    os.mkdir(plot_metrics_path)
 # construct path to params.yaml file
 relative_param_path = "../params.yaml"
 param_path = os.path.join(file_path,relative_param_path)
@@ -21,9 +28,11 @@ params = yaml.safe_load(open(param_path))["run_model"]
 dp = params["data_path"]
 feature_ids = params["feature_ids"]
 model_name = params["model_name"]
+save_traces = params["save_traces"]
 # load data
 dh = PyTimsDataHandleDDA(dp)
-
+metrics_dictionary = {}
+metrics_plot_list = []
 for feature_id in feature_ids:
     aligned_features = AlignedFeatureData(
                                 dh,
@@ -37,7 +46,7 @@ for feature_id in feature_ids:
         model = ModelM2(aligned_features)
     else:
         raise ValueError("Unknown model name.")
-    model_trace = model.evaluation(prior_pred_in=True,
+    model_trace: InferenceData = model.evaluation(prior_pred_in=True,
                      prior_pred_out=True,
                      posterior_pred_in=True,
                      posterior_pred_out=True,
@@ -45,6 +54,27 @@ for feature_id in feature_ids:
                      write_to_file=True,
                      folder_path=plot_path)
     model.arviz_plots(path=plot_path)
-    model_trace.to_json(f"{metrics_path}/{feature_id}_idata.json")
+    if save_traces:
+        model_trace.to_netcdf(f"{idata_path}/{feature_id}_idata.nc")
+    stats = model_trace.sample_stats
+    feature_dictionary = {}
+    feature_dictionary["divergences"] = int(stats.diverging.values.sum())
+    feature_dictionary["acceptance_rate"] = float(stats.acceptance_rate.values.mean())
+    metrics_dictionary[feature_id] = feature_dictionary
 
-
+    acc_rate = stats.acceptance_rate.values.flatten().tolist()
+    tree_depth = stats.tree_depth.values.flatten().tolist()
+    n_steps = stats.n_steps.values.flatten().tolist()
+    step_size = stats.step_size.values.flatten().tolist()
+    metrics_plot_list += [{"feature_id":feature_id,"tree_depth":td,"acceptance_rate":ar,"n_steps":ns,"step_size":ss} for (td,ar,ns,ss) in zip(acc_rate,tree_depth,n_steps,step_size) ]
+    
+    
+    
+with open(f"{metrics_path}/metrics.json", "w") as json_file:
+    jf = json.dumps(metrics_dictionary,indent=4)
+    json_file.write(jf)
+with open(f"{plot_metrics_path}/metrics_plot_dict.json","w") as json_file:
+    jf = json.dumps({
+        "plot_metrics": metrics_plot_list
+    },indent=4)
+    json_file.write(jf)
